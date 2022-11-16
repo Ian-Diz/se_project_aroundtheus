6902483f659a1ba248c6199b7a79c1ad37ab60c1
Ian <ifd730@gmail.com>
2022-11-16 13:09:34 -0800

    First submission Stage 1 project four

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let initialCards = [object1, object2, object3, object4, object5, object6];
+
+let object1 = {
+  name: "Yosemite Valley",
+  link: "../images/like.svg",
+};
+
+let object2 = {
+  name: "Lake Louise",
+  link: "../images/lake-louiselake.svg",
+};
+
+let object3 = {
+  name: "Bald Mountains",
+  link: "../images/bald-mountainsbald.svg",
+};
+
+let object4 = {
+  name: "Latemar",
+  link: "../images/latemarlatemar.svg",
+};
+
+let object5 = {
+  name: "Vanoise National Park",
+  link: "../images/vanoise-national-parkvanoise.svg",
+};
+
+let object6 = {
+  name: "Lago di Braies",
+  link: "../images/lago-di-braieslago.svg",
+};
