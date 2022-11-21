edd62b22e9c5f145e0bb9d49d0acd6a1c701829b
Ian <ifd730@gmail.com>
2022-11-21 11:46:00 -0800

    Third submission to stage four project four

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,59 @@
-let object1 = {
-  name: "Yosemite Valley",
-  link: "./images/yosemite-valleyyosemite.svg",
-};
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "./images/yosemite-valleyyosemite.svg",
+  },
 
-let object2 = {
-  name: "Lake Louise",
-  link: "./images/lake-louiselake.svg",
-};
+  {
+    name: "Lake Louise",
+    link: "./images/lake-louiselake.svg",
+  },
 
-let object3 = {
-  name: "Bald Mountains",
-  link: "./images/bald-mountainsbald.svg",
-};
+  {
+    name: "Bald Mountains",
+    link: "./images/bald-mountainsbald.svg",
+  },
+  {
+    name: "Latemar",
+    link: "./images/latemarlatemar.svg",
+  },
 
-let object4 = {
-  name: "Latemar",
-  link: "./images/latemarlatemar.svg",
-};
+  {
+    name: "Vanoise National Park",
+    link: "./images/vanoise-national-parkvanoise.svg",
+  },
 
-let object5 = {
-  name: "Vanoise National Park",
-  link: "./images/vanoise-national-parkvanoise.svg",
-};
+  {
+    name: "Lago di Braies",
+    link: "./images/lago-di-braieslago.svg",
+  },
+];
 
-let object6 = {
-  name: "Lago di Braies",
-  link: "./images/lago-di-braieslago.svg",
-};
+const main = document.querySelector(".main");
 
-let main = document.querySelector(".main");
+const profile = main.querySelector(".profile");
 
-let profile = main.querySelector(".profile");
+const editButton = profile.querySelector(".profile__edit");
 
-let editButton = profile.querySelector(".profile__edit");
+const editProfile = main.querySelector(".edit-profile");
 
-let editProfile = profile.querySelector(".edit-profile");
+const closeButton = editProfile.querySelector(".edit-profile__close");
 
-let closeButton = editProfile.querySelector(".edit-profile__close");
+const form = editProfile.querySelector(".edit-profile__form");
 
-let profileName = profile.querySelector(".edit-profile__name");
+let profileName = editProfile.querySelector(".edit-profile_input_name");
 
-let profileDesc = profile.querySelector(".edit-profile__description");
+let profileDesc = editProfile.querySelector(".edit-profile_input_description");
 
 let profileTitle = profile.querySelector(".profile__title");
 
 let profileSubtitle = profile.querySelector(".profile__subtitle");
 
-let saveButton = editProfile.querySelector(".edit-profile__save");
+const saveButton = editProfile.querySelector(".edit-profile__save");
 
-let cards = main.querySelector(".cards");
+const cards = main.querySelector(".cards");
 
 let card = cards.querySelectorAll(".card");
-
-let initialCards = [object1, object2, object3, object4, object5, object6];
 
 function openEdit() {
   profileName.value = profileTitle.textContent;
@@ -68,11 +69,11 @@ function saveEdit(event) {
   event.preventDefault();
   profileTitle.textContent = profileName.value;
   profileSubtitle.textContent = profileDesc.value;
-  editProfile.classList.remove("edit-profile_opened");
+  closeEdit();
 }
 
 closeButton.addEventListener("click", closeEdit);
-saveButton.addEventListener("click", saveEdit);
+form.addEventListener("submit", saveEdit);
 editButton.addEventListener("click", openEdit);
 
 function getCardElement(data) {
@@ -81,28 +82,11 @@ function getCardElement(data) {
   let cardTitle = cardElement.querySelector(".card__title");
   cardImage.src = data.link;
   cardImage.alt = data.name;
-  cardTitle.value = data.name;
+  cardTitle.textContent = data.name;
+  return cardElement;
 }
 
 for (i = 0; i < initialCards.length; ++i) {
-  getCardElement(initialCards[i]);
-  cards.insertAdjacentHTML(
-    "beforeend",
-    `
-    <article class="card">
-      <img
-        class="card__image"
-        alt="${initialCards[i].name}"
-        src="${initialCards[i].link}"
-      />
-      <h2 class="card__title">${initialCards[i].name}</h2>
-      <button class="card__like" aria-label="Like" type="Button">
-        <img
-         alt="A heart-shaped like button to show your appreciation"
-          src="./images/like.svg"
-        />
-       </button>
-  </article>
-  `
-  );
+  let insertCard = getCardElement(initialCards[i]);
+  cards.prepend(insertCard);
 }
